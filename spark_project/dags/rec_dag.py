d221a5b772cb35bea70950583828902a70095cba
Vladimir Ponomarev <106449943+PonomarevVladimir@users.noreply.github.com>
2023-07-05 14:24:23 +0300

    Upload dags

---
+++ spark_project/dags/rec_dag.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.decorators import dag
+from airflow.models import Variable
+from datetime import date
+from datetime import datetime
+import pendulum
+
+input_path = Variable.get('INPUT_PATH') # hdfs://rc1a-dataproc-m-dg5lgqqm7jju58f9.mdb.yandexcloud.net:8020/user/vladimirpo/data/geo/
+
+def calculate_number_of_days():
+    DATA_START_DATE =  datetime.strptime(Variable.get('DATA_START_DATE'), "YYYY-mm-dd").date()
+    oper_dt = date.today()
+    return oper_dt - DATA_START_DATE
+
+with DAG(
+        'dag_get_file',
+        description='sprint7_project',
+        start_date=pendulum.parse('2023-02-06'),
+        schedule_interval="@daily",
+        max_active_runs = 1,
+) as dag:
+    task = BashOperator(
+        task_id="rec",
+        bash_command = f"spark-submit src\\scripts\\rec.py '{date.today()}' '{calculate_number_of_days}' 'append' '{input_path}'",
+	    dag = dag
+    )
+
+    (
+    task
+    )
---
+++ spark_project/dags/zs_dag.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.decorators import dag
+from airflow.models import Variable
+from datetime import date
+import pendulum
+
+input_path = Variable.get('INPUT_PATH') # hdfs://rc1a-dataproc-m-dg5lgqqm7jju58f9.mdb.yandexcloud.net:8020/user/vladimirpo/data/geo/
+
+with DAG(
+        'dag_zs',
+        description='sprint7_project',
+        start_date=pendulum.parse('2023-02-06'),
+        schedule_interval="@weekly",
+        max_active_runs = 1,
+) as dag:
+    task = BashOperator(
+        task_id='prepare_zone_stats',
+        bash_command = f"spark-submit src\\scripts\\zs.py '{date.today()}' '37' 'append' '{input_path}'",
+        dag = dag
+    )
+
+    (
+    task
+    )
